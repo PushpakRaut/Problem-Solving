ef1af16b2fddb41397a9813e8734292350a5d78d
Pushpak Raut <pushpakraut123@gmail.com>
2025-07-29 01:25:48 +0530

    ArrayMedium

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 //        cn.printAllDivisors(36);
 //        BasicRecursion br = new BasicRecursion();
 //        br.fibonacciNumber(5);
-        String str = "aba";
+//        String str = "aba";
 //        System.out.println(br.checkStringPalindromeReursive(str, 0, str.length() - 1));
 //        int[] arr = {4,6,8,2,1,0,7};
 //        System.out.println(Arrays.toString(br.reverseArrayRecursive(arr, 0, arr.length - 1)));
@@ -24,15 +24,22 @@ public class Main {
 //        System.out.println(Arrays.toString(sII.quickSort(arr, 0, arr.length - 1)));
 //        System.out.println(Arrays.toString(sII.recursiveInsertionSort(arr, 1)));
 
-        ArrayEasy ae = new ArrayEasy();
+//        ArrayEasy ae = new ArrayEasy();
 //        System.out.println(Arrays.toString(ae.findSecondSmallestAndLargest(arr)));
-        int[] arr = {1,2,0,2,3,0,0,3,0,4,5};
-        int arr1[] = {1,2,3,4,5};
-        int arr2[] = {2,3,4,0,5};
-        int arr3[] = {1,1,0,1,1,1};
-        int[] arr4 = {2,3,9,-1, 1, 1};
+//        int[] arr = {1,2,0,2,3,0,0,3,0,4,5};
+//        int arr1[] = {1,2,3,4,5};
+//        int arr2[] = {2,3,4,0,5};
+//        int arr3[] = {1,1,0,1,1,1};
+//        int[] arr4 = {2,3,9,-1, 1, 1};
 //        System.out.println(Arrays.toString(ae.unionTwoSortedArray(arr1, arr2)));
 //        System.out.println(ae.linearSearch(arr, 57));
-        System.out.println(ae.longestSubarrayWithKSumPositiveAndNegative(arr4, 10));
+//        System.out.println(ae.longestSubarrayWithKSumPositiveAndNegative(arr4, 10));
+
+        ArrayMedium am = new ArrayMedium();
+        int[] arr = {2,3,9,-1, 1, 1};
+        int[] arr1 = {0,1,2,1,1,1,0,0,2,0,2,2,1};
+        int[] arr2 = {7,7,5,7,5,1,5,7,5,5,7,7,5,5,5,5};
+//        System.out.println(Arrays.toString(am.sortArray0s1s2s(arr1)));
+        System.out.println(am.majorityElementNby2(arr2));
     }
 }
---
+++ src/StriverA2Z/ArrayMedium.java
@@ -0,0 +1,176 @@
+package StriverA2Z;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayMedium {
+
+    public int[] twoSum(int[] arr, int k) {
+//        for(int i = 0;i<arr.length;i++) {
+//            for(int j = 0;j<arr.length;j++) {
+//                if(arr[i] + arr[j] == k) return new int[]{i, j};
+//            }
+//        }
+//        return new int[]{-1,-1};
+
+
+        //2nd solution
+//        Map<Integer, Integer> map = new HashMap<>();
+//
+//        for(int i=0;i<arr.length;i++) {
+//            if(map.containsKey(k - arr[i])) {
+//                return new int[]{i, map.get(k-arr[i])};
+//            }
+//
+//            map.put(arr[i], i);
+//        }
+//        return new int[]{-1, -1};
+
+        //3rd solution - two pointer
+        int left = 0, right = arr.length - 1;
+        while(left < right) {
+            int sum = arr[left] + arr[right];
+            if(sum == k) {
+                return new int[]{left, right};
+            } else if(sum < k) left++;
+            else right++;
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int[] sortArray0s1s2s(int[] arr) {
+        int n = arr.length;
+
+//        mergeSort(arr, 0, n-1);
+
+        //2nd solution
+//        int a=0,b=0,c=0;
+//        for(int e:arr){
+//            if(e==0)a++;
+//            else if(e==1)b++;
+//            else c++;
+//        }
+//
+//        for(int i=0;i<a;i++) arr[i]=0;
+//        for(int i=a;i<a+b;i++) arr[i]=1;
+//        for(int i=a+b;i<n;i++) arr[i]=2;
+
+
+        //3rd solution - Dutch national flag algorithm
+        int low = 0, mid = 0, high= n - 1;
+
+        while(mid<=high) {
+            if(arr[mid] == 0){
+                swap(arr, mid, low);
+                low++;
+                mid++;
+            } else if(arr[mid] == 1){
+                mid++;
+            }else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+        return arr;
+    }
+    private void  swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    private void mergeSort(int[] arr, int low, int high) {
+        if(low<high) {
+            int mid = (high + low) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid+1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+    private void merge(int[] arr, int low, int mid, int high) {
+        int n = mid - low + 1;
+        int m = high - mid;
+
+        int[] l = new int[n];
+        int[] r = new int[m];
+
+        for(int x = 0;x<n;x++) {
+            l[x] = arr[low+x];
+        }
+
+        for(int x=0;x<m;x++) {
+            r[x] = arr[mid+1+x];
+        }
+
+        int i =0, j=0, k=low;
+        while(i<n&&j<m) {
+            if(l[i]<r[j]){
+                arr[k]=l[i];
+                i++;
+            } else{
+                arr[k]=r[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n){
+            arr[k]=l[i];
+            i++;
+            k++;
+        }
+        while(j<m){
+            arr[k]=r[j];
+            j++;
+            k++;
+        }
+    }
+
+
+    public int majorityElementNby2(int[] arr) {
+        int n = arr.length;
+//
+//        for(int i = 0;i<n;i++) {
+//            int count= 0;
+//            for(int j = 0;j<n;j++) {
+//                if(arr[i] == arr[j]) count++;
+//            }
+//            if(count > n/2) {
+//                return arr[i];
+//            }
+//        }
+//        return -1;
+
+        //2nd solution
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for(int e:arr) {
+//            map.put(e, map.getOrDefault(e, 0)+1);
+//        }
+//
+//        for(Map.Entry<Integer, Integer> e: map.entrySet()) {
+//            if(e.getValue() > n/2) return e.getKey();
+//        }
+//
+//        return -1;
+
+        //3rd solution - Moore's voting algo
+        int el = -1;
+        int count = 0;
+        for(int e:arr) {
+            if(count == 0) {
+                el = e;
+                count = 1;
+            } else if(el == e) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        count = 0;
+        for(int e: arr) {
+            if(el == e) count++;
+        }
+        if(count > n/2) return el;
+        return -1;
+    }
+
+}
